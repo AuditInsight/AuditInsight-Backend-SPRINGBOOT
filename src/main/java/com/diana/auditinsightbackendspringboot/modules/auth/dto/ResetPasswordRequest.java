22a3d1aa1518ff2d74a9640025c41f5d462772e7
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-25 19:30:35 +0200

    Reset password file and imported lombok and jakarta

---
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/dto/ResetPasswordRequest.java
@@ -0,0 +1,8 @@
+package com.diana.auditinsightbackendspringboot.modules.auth.dto;
+
+import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class ResetPasswordRequest {
+}
