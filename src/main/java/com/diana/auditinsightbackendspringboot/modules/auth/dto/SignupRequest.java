88ab1be26477929a62a84ef6229d47f3ac37e0b0
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-25 19:10:00 +0200

    imported validations constraints needed

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/dto/SignupRequest.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/dto/SignupRequest.java
@@ -1,4 +1,9 @@
 package com.diana.auditinsightbackendspringboot.modules.auth.dto;
 
+import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class SignupRequest {
 }
