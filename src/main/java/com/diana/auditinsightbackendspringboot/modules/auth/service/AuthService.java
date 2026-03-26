c7f956a706803d8f3ff991c341c2c89bffa380b8
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-26 09:24:22 +0200

    added a spring security dependency

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/service/AuthService.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/auth/service/AuthService.java
@@ -4,8 +4,14 @@ import com.diana.auditinsightbackendspringboot.modules.auth.dto.*;
 import com.diana.auditinsightbackendspringboot.modules.auth.entity.User;
 import com.diana.auditinsightbackendspringboot.modules.auth.otp.OtpEntity;
 import com.diana.auditinsightbackendspringboot.modules.auth.entity.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final OtpService otpService;
+
 }
