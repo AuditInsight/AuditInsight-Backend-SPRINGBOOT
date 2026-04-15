316d0a6e4202151aa254646cc46cbe20437f6fe2
Diana BUYINZA <buyinzadiana@gmail.com>
2026-04-15 14:40:44 +0200

    transactions repository

--- src/main/java/com/diana/auditinsightbackendspringboot/modules/transactions/dto/TransactionRequest.java
+++ src/main/java/com/diana/auditinsightbackendspringboot/modules/transactions/dto/TransactionRequest.java
@@ -1,4 +1,22 @@
 package com.diana.auditinsightbackendspringboot.modules.transactions.dto;
 
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
 public class TransactionRequest {
+
+    private LocalDate date;
+
+    private Double amount;
+
+    private String counterparty;
+
+    private String type;
+
+    private String source;
+
+    private String status;
+
+    private Integer riskScore;
 }
